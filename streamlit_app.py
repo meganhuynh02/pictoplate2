d3faa824d4098963e463dd8f70b662d0ecfd48f8
Megan Huynh <62710899+meganhuynh02@users.noreply.github.com>
2024-01-31 13:32:26 -0800

    added imports, trying to display uploaded image

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 
 import streamlit as st
 from openai import OpenAI
+import numpy as np
+import cv2
 
 # create website sidebar
 with st.sidebar:
@@ -27,6 +29,13 @@ if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "What should we cook today? :yum:"}]
 
 uploaded_file = st.file_uploader("Upload a picture of the ingredients you have")
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+
+    # Now do something with the image! For example, let's display it:
+    st.image(opencv_image, channels="BGR")
 
 if uploaded_file and not openai_api_key:
     st.info("Please add your OpenAI API key to continue.")
