664edc638f1121c6ddf5ecf144f8b72ce9db881c
Megan Huynh <62710899+meganhuynh02@users.noreply.github.com>
2024-01-31 13:19:48 -0800

    changed gpt model

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if prompt := st.chat_input():
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-4-turbo", messages=st.session_state.messages)
+    response = client.chat.completions.create(model="gpt-4-vision-preview", messages=st.session_state.messages)
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
